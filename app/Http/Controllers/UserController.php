78716216f3b3d9029c93b596a695a3f742c83666
Diaz Linggaputra <diazlinggaputra@gmail.com>
2023-05-24 23:20:24 +0800

    feat: add user login and register endpoint

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'fullName' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = User::create([
+            'fullName' => $request->fullName,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('AuthToken')->plainTextToken;
+
+            return response()->json(['message' => 'User logged in successfully', 'user' => $user, 'token' => $token]);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+    }
+}
