c53597e65d1020be762df1924a2e737c0fa66f18
Diaz Linggaputra <diazlinggaputra@gmail.com>
2023-05-29 16:54:50 +0800

    chore: refactoring codes that gets returned so it's uniform and easier to read

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -13,57 +13,82 @@ class NewsController extends Controller
         $query = $request->query();
 
         // Construct the NewsAPI URL with query parameters
-        $url = 'https://newsapi.org/v2/top-headlines?' . http_build_query($query + ['apiKey' => config('services.news_api.key')]);
-        
+        $url = 'https://newsapi.org/v2/top-headlines?country=us&sortBy=popularity&' . http_build_query($query + ['apiKey' => config('services.news_api.key')]);
+
         // Make the HTTP request to the NewsAPI
         $response = Http::get($url);
         
-        // Return the response from NewsAPI
-        return $response->json();
+        $filteredArticles = collect($response->json('articles'))->filter(function ($article) {
+            return (
+                Arr::get($article, 'source.name') &&
+                $article['author'] &&
+                $article['title'] &&
+                $article['description'] &&
+                $article['url'] &&
+                $article['urlToImage'] &&
+                $article['publishedAt']
+            );
+        });
+
+        // Map the filtered articles to the desired format
+        $news = $filteredArticles->map(function ($article) {
+            return [
+                'title' => strpos($article['title'], '-') !== false ? trim(substr($article['title'], 0, strrpos($article['title'], '-'))): $article['title'],
+                'source' => Arr::get($article, 'source.name'),
+                'author' => $article['author'],
+                'description' => $article['description'],
+                'url' => $article['url'],
+                'imageUrl' => $article['urlToImage'],
+                'createdDate' => $article['publishedAt'],
+            ];
+        })->values()->all();
+
+        // Return the filtered and mapped articles
+        return $news;
     }
 
     public function fetchNewsApiFilter(Request $request)
     {
-    $query = $request->query();
+        $query = $request->query();
 
-    // Construct the NewsAPI URL with query parameters
-    $url = 'https://newsapi.org/v2/everything?' . http_build_query($query + ['apiKey' => config('services.news_api.key')]);
-    
-    // Make the HTTP request to the NewsAPI
-    $response = Http::get($url);
-    
-    // Extract the filtered articles from the response
-    $filteredArticles = collect($response->json('articles'))->filter(function ($article) {
-        return (
-            Arr::get($article, 'source.name') &&
-            $article['author'] &&
-            $article['title'] &&
-            $article['description'] &&
-            $article['url'] &&
-            $article['urlToImage'] &&
-            $article['publishedAt']
-        );
-    });
+        // Construct the NewsAPI URL with query parameters
+        $url = 'https://newsapi.org/v2/everything?' . http_build_query($query + ['apiKey' => config('services.news_api.key')]);
+        
+        // Make the HTTP request to the NewsAPI
+        $response = Http::get($url);
+        
+        // Extract the filtered articles from the response
+        $filteredArticles = collect($response->json('articles'))->filter(function ($article) {
+            return (
+                Arr::get($article, 'source.name') &&
+                $article['author'] &&
+                $article['title'] &&
+                $article['description'] &&
+                $article['url'] &&
+                $article['urlToImage'] &&
+                $article['publishedAt']
+            );
+        });
 
-    // Map the filtered articles to the desired format
-    $news = $filteredArticles->map(function ($article) {
-        return [
-            'title' => strpos($article['title'], '-') !== false ? trim(substr($article['title'], 0, strrpos($article['title'], '-'))): $article['title'],
-            'source' => Arr::get($article, 'source.name'),
-            'author' => $article['author'],
-            'description' => $article['description'],
-            'url' => $article['url'],
-            'imageUrl' => $article['urlToImage'],
-            'createdDate' => $article['publishedAt'],
-        ];
-    })->slice(0, 20)->all();
+        // Map the filtered articles to the desired format
+        $news = $filteredArticles->map(function ($article) {
+            return [
+                'title' => strpos($article['title'], '-') !== false ? trim(substr($article['title'], 0, strrpos($article['title'], '-'))): $article['title'],
+                'source' => Arr::get($article, 'source.name'),
+                'author' => $article['author'],
+                'description' => $article['description'],
+                'url' => $article['url'],
+                'imageUrl' => $article['urlToImage'],
+                'createdDate' => $article['publishedAt'],
+            ];
+        })->slice(0, 20)->values()->all();
 
-    // Return the filtered and mapped articles
-    return response()->json($news);
+        // Return the filtered and mapped articles
+        return $news;
     }
 
 
-    public function fetchGuardianApiHome(Request $request)
+     public function fetchGuardianApiHome(Request $request)
     {
         $query = $request->query();
 
@@ -73,46 +98,71 @@ class NewsController extends Controller
         // Make the HTTP request to the The Guardian API
         $response = Http::get($url);
         
-        // Return the response from The Guardian API
-        return $response->json();
+        // Extract the filtered articles from the response
+        $filteredArticles = collect($response->json('response.results'))->filter(function ($article) {
+            return (
+                $article['webTitle'] &&
+                Arr::get($article, 'fields.trailText') &&
+                $article['webUrl'] &&
+                Arr::get($article, 'fields.thumbnail') &&
+                $article['webPublicationDate']
+            );
+        });
+
+        // Map the filtered articles to the desired format
+        $news = $filteredArticles->map(function ($article) {
+            return [
+                'title' => $article['webTitle'],
+                'source' => 'The Guardian',
+                'author' => 'The Guardian',
+                'description' => Arr::get($article, 'fields.trailText'),
+                'imageUrl' => Arr::get($article, 'fields.thumbnail'),
+                'url' => $article['webUrl'],
+                'createdDate' => $article['webPublicationDate'],
+                'category' => $article['sectionId']
+            ];
+        })->values()->all();
+
+        // Return the filtered and mapped articles
+        return response()->json($news);
     }
 
     public function fetchGuardianApiFilter(Request $request)
     {
-    $query = $request->query();
+        $query = $request->query();
 
-    // Construct the The Guardian API URL with query parameters
-    $url = 'https://content.guardianapis.com/search?show-fields=thumbnail,trailText&page-size=20&' . http_build_query($query + ['api-key' => config('services.guardian_api.key')]);
-    
-    // Make the HTTP request to the The Guardian API
-    $response = Http::get($url);
-    
-    // Extract the filtered articles from the response
-    $filteredArticles = collect($response->json('response.results'))->filter(function ($article) {
-        return (
-            $article['webTitle'] &&
-            Arr::get($article, 'fields.trailText') &&
-            $article['webUrl'] &&
-            Arr::get($article, 'fields.thumbnail') &&
-            $article['webPublicationDate']
-        );
-    });
+        // Construct the The Guardian API URL with query parameters
+        $url = 'https://content.guardianapis.com/search?show-fields=thumbnail,trailText&page-size=20&' . http_build_query($query + ['api-key' => config('services.guardian_api.key')]);
+        
+        // Make the HTTP request to the The Guardian API
+        $response = Http::get($url);
+        
+        // Extract the filtered articles from the response
+        $filteredArticles = collect($response->json('response.results'))->filter(function ($article) {
+            return (
+                $article['webTitle'] &&
+                Arr::get($article, 'fields.trailText') &&
+                $article['webUrl'] &&
+                Arr::get($article, 'fields.thumbnail') &&
+                $article['webPublicationDate']
+            );
+        });
 
-    // Map the filtered articles to the desired format
-    $news = $filteredArticles->map(function ($article) {
-        return [
-            'title' => $article['webTitle'],
-            'source' => 'The Guardian',
-            'author' => 'The Guardian',
-            'description' => Arr::get($article, 'fields.trailText'),
-            'imageUrl' => Arr::get($article, 'fields.thumbnail'),
-            'url' => $article['webUrl'],
-            'createdDate' => $article['webPublicationDate'],
-        ];
-    })->all();
+        // Map the filtered articles to the desired format
+        $news = $filteredArticles->map(function ($article) {
+            return [
+                'title' => $article['webTitle'],
+                'source' => 'The Guardian',
+                'author' => 'The Guardian',
+                'description' => Arr::get($article, 'fields.trailText'),
+                'imageUrl' => Arr::get($article, 'fields.thumbnail'),
+                'url' => $article['webUrl'],
+                'createdDate' => $article['webPublicationDate'],
+            ];
+        })->values()->all();
 
-    // Return the filtered and mapped articles
-    return response()->json($news);
+        // Return the filtered and mapped articles
+        return response()->json($news);
     }
 
     public function fetchNYTApiHome()
@@ -122,9 +172,35 @@ class NewsController extends Controller
         
         // Make the HTTP request to the New York Times API
         $response = Http::get($url);
+
+         // Extract the filtered articles from the response
+         $filteredArticles = collect($response->json('results'))->filter(function ($article) {
+            return (
+                $article['title'] &&
+                $article['abstract'] &&
+                Arr::get($article, 'multimedia.0.url') &&
+                $article['url'] &&
+                $article['created_date'] 
+            );
+        });
+
         
-        // Return the response from New York Times API
-        return $response->json();
+        // Map the filtered articles to the desired format
+        $news = $filteredArticles->map(function ($article) {
+            return [
+                'title' => $article['title'],
+                'source' => 'The New York Times',
+                'author' => 'The New York Times',
+                'description' => $article['abstract'],
+                'imageUrl' => Arr::get($article, 'multimedia.0.url'),
+                'url' => $article['url'],
+                'createdDate' => $article['created_date'],
+                'category' => $article['section'],
+            ];
+        })->values()->all();
+
+        // Return the filtered and mapped articles
+        return response()->json($news);
     }
 
     public function fetchNYTApiFilter(Request $request)
